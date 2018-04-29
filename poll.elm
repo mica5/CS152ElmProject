23b2785cb5c050a6f8cbc8da40ce7912099b2e40
MarkeyMarke <casapao.markkenneth@gmail.com>
2018-04-28 18:18:16 -0700

    Added lots of comments to be able to understand the code's meaning easier. Also renamed a few variables and combined redundant functions.

--- poll.elm
+++ poll.elm
@@ -6,21 +6,23 @@ import Html.Attributes exposing (..)
 main =
   Html.beginnerProgram { model = model, view = view, update = update }
 
---Define Model
+--Define Model. Model is similar to a struct in C, or an object in JS, but it is immutable. Elm gets around this by
+--Generating a new copy of that object but which shares memory with the original record, except for the changed values.
 type alias Model =
   { question : String
-  , choice1 : String
-  , choice2 : String
-  , choice3 : String
-  , choice4 : String
-  , answer : Int
+  --String values for questions
+  , questionerChoice1 : String
+  , questionerChoice2 : String
+  , questionerChoice3 : String
+  , questionerChoice4 : String
+  --answer index (1 for first answer, x for xth answer)
+  , answerIndex : Int
   }
 
 --MODEL (Data)
-
 --Set as Type Model
 model : Model
---Instance Variables , Instanciate
+--Instance Variables , Instantiate
 model =
   Model "Your question will show here." "A1 goes here." "A2 goes here." "A3 goes here." "A4 goes here." 1
 
@@ -31,34 +33,44 @@ CS152 Project
 
 --UPDATE (Change Data)
 -- Define Msg
--- Msg is signal to the system
+-- Msg is signal to the system, we use case matching to allow the view section to give a particular signal
 type Msg = 
-  Submit 
-  | SetQuestion String
-  | SetAnswer Int String
-  | SetCorrectAnswer Int
+  Submit --Submits the current model values and updates it for the answerer's POV
+  | SetQuestion String --sets a new question string
+  | SetAnswer Int String --sets a new answer string
+  | SetCorrectAnswer Int --sets a new answer index (1 for first answer, x for xth answer)
 
+--Update will take a message signal based on what kind of messgae the view section has given
+--The common property with all of them is that they may pass parameters of their own, and they replace the model data with
+--an updated copy of itself.
 update : Msg -> Model -> Model
+--Now we're defining the function call for update. We're naming the first two parameters msg and model.
+--Note that msg is a type Msg which is given several types as shown above, and that is handy for case matching
+--Then, all that is left is what kind of changes to the model are made.
 update msg model =
   case msg of
     Submit -> model
     SetQuestion q -> {model | question = q}
     SetAnswer int ans-> 
       case int of
-      --Generate Clone Copy of Model
-      1 -> { model | choice1 = ans}
-      2 -> { model | choice2 = ans}
-      3 -> { model | choice3 = ans}
-      4 -> { model | choice4 = ans}
-      _ -> { model | choice1 = ans}
-    SetCorrectAnswer num -> {model | answer = num}
+      --Generate Updated Clone Copy of Model, this is Elm's equivalent to using a setter for an object
+      1 -> { model | questionerChoice1 = ans}
+      2 -> { model | questionerChoice2 = ans}
+      3 -> { model | questionerChoice3 = ans}
+      4 -> { model | questionerChoice4 = ans}
+      _ -> { model | questionerChoice1 = ans} --We're setting choice 1 as the default choice if anything goes wrong
+    SetCorrectAnswer num -> {model | answerIndex = num}
 
---VIEW (Print out the data)
+{-VIEW (Print out the data)
+View's job is to interpret the model every time it is updated. We will take the date from model and display it in GUI
+View's second job is to provide controls to the user to update the model. This is done through text fields and buttons
+of all kinds. They will take input, and then pass a message to update. It is up for us to decide what parameters are
+necessary and how it works. -}
 view : Model -> Html Msg
 view model =
-  div []
+  div [] --View is literally one giant hierarchy of HTML. The first [] is for attributes, and the second [] is for content.
     [ 
-      br [] []
+      br [] [] --We use an empty br to break down into a new line, or provide spacing.
     , input [placeholder "Enter your question here.", onInput SetQuestion] []
     , question "question" "A1" 1
     , question "question" "A2" 2
@@ -66,30 +78,36 @@ view model =
     , question "question" "A4" 4
     , button [ onClick Submit ] [ text "Submit" ]
     , br [] [] , br [] []
-    , div [] [text model.question]
-    , div [] [text model.choice1]
-    , div [] [text model.choice2]
-    , div [] [text model.choice3]
-    , div [] [text model.choice4]
+    , modelValues
     , br [] []
-    , div [] [text (toString model.answer)]
     ]
 
--- Radio Button
-radio : String -> String -> Int -> Html Msg
-radio groupName value msg =
-  label
-    [ style [("padding", "20px")]
-    ]
-    [ input [ type_ "radio", name groupName, onClick (SetCorrectAnswer msg)] []
-    , text value
+{-This is a function that takes no parameters and displays the current values of the model. This is not part of the main
+application and is more useful for just debugging in general. -}
+modelValues : Html Msg
+modelValues = 
+  fieldset []
+    [
+      div [] [text model.question]
+    , div [] [text model.questionerChoice1]
+    , div [] [text model.questionerChoice2]
+    , div [] [text model.questionerChoice3]
+    , div [] [text model.questionerChoice4]
+    , div [] [text ("The current answer is " ++ (toString model.answerIndex) ++ ".")]
     ]
 
 -- (Radio Button + Text Box)
--- Takes in String and Int
+{- Radio Buttons use groupNames to exclude other button in the same group, textValue to provide a text paired up with it,
+and newAnswerIndex which takes an int for the new answer index-}
+-- Takes in groupName to pass into radio, takes in a textValue to assign that button, newAnswerIndex is the new answer
 question : String -> String -> Int -> Html Msg
-question groupName name num =
+question groupName textValue newAnswerIndex =
   fieldset []
-        [ radio groupName name (num)
-        , input [ placeholder "Enter your answer here.", onInput (SetAnswer num)] []
+        [ 
+          --beginning of radio button
+          label
+           [ style [("padding", "20px")]]
+           [ input [ type_ "radio", name groupName, onClick (SetCorrectAnswer newAnswerIndex)] [], text textValue]
+          --ending of radio button
+        , input [ placeholder "Enter your answer here.", onInput (SetAnswer newAnswerIndex)] []
         ]
